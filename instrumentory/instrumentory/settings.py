1791b74bbe7af4d508c0516125e341b6570ab995
Alec <robert.hovey95@gmail.com>
2022-09-13 11:00:14 -0400

    reset envoron call in settings

--- instrumentory/instrumentory/settings.py
+++ instrumentory/instrumentory/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 import environ
 
 
-env = environ.Env().get_value
+env = environ.Env()
 environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
