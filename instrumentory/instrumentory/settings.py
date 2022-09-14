d30b877c0246ce9c5fd523bf18dfeb3cc07068da
Alec <robert.hovey95@gmail.com>
2022-09-14 10:43:05 -0400

    removed base_dir from templates

--- instrumentory/instrumentory/settings.py
+++ instrumentory/instrumentory/settings.py
@@ -118,7 +118,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'frontend/build'
+            #BASE_DIR / 'frontend/build'
+            'frontend/build'
         ],
         'APP_DIRS': True,
         'OPTIONS': {
