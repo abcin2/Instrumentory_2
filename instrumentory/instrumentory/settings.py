a9d8065c1ae42685d0da364e80d334cf10d55a03
Alec <robert.hovey95@gmail.com>
2022-09-14 12:22:35 -0400

    created build folders in multiple places

--- instrumentory/instrumentory/settings.py
+++ instrumentory/instrumentory/settings.py
@@ -200,8 +200,9 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'frontend/build/static'
-    #'/frontend/build/static'
+    BASE_DIR / 'frontend/build/static',
+    '/frontend/build/static',
+    '/build/static'
 ]
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
