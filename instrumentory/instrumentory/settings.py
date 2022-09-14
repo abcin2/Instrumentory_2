685d875ef12111dbde77b051d3ffec6b985dea39
Alec <robert.hovey95@gmail.com>
2022-09-14 15:26:32 -0400

    moved build and changed templates location

--- instrumentory/instrumentory/settings.py
+++ instrumentory/instrumentory/settings.py
@@ -118,7 +118,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'frontend/build'
+            BASE_DIR / 'staticfiles/build'
         ],
         'APP_DIRS': True,
         'OPTIONS': {
