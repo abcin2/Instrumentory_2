0001503183fab8e7498990889c6a325aecd817b5
Alec <robert.hovey95@gmail.com>
2022-09-14 10:33:31 -0400

    changing dirs again removing BASE_DIR

--- instrumentory/instrumentory/settings.py
+++ instrumentory/instrumentory/settings.py
@@ -200,7 +200,8 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'frontend/build/static'
+    #BASE_DIR / 'frontend/build/static'
+    '/frontend/build/static'
 ]
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
