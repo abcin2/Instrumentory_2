1c6fd06899fa5c9ae2f1673480e6dec3ecfdfea6
Robert Hovey <robert.hovey95@gmail.com>
2022-09-17 09:43:39 -0400

    changed base_dir and updated base_url for npm run build script

--- instrumentory/instrumentory/settings.py
+++ instrumentory/instrumentory/settings.py
@@ -118,7 +118,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'staticfiles/build'
+            BASE_DIR / 'frontend/build'
         ],
         'APP_DIRS': True,
         'OPTIONS': {
